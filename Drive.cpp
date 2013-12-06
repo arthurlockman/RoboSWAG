ffa1175439072311d26f4c9f7cb9ae5e70d2ab9f
Arthur Lockman <hello@rthr.me>
2013-12-05 21:45:52 -0500

    -Added doxygen support. -Drive class is now fully documented. -Robotmap is documented for the things that are currently in it.

--- Drive.cpp
+++ Drive.cpp
@@ -1,6 +1,22 @@
 #include "Arduino.h"
 #include "Drive.h"
 
+/**
+ * @brief Constructs the robot drive object.
+ * @details This method attaches all of the 
+ * servo motors to their proper pins, and 
+ * finishes construction of the drive object.
+ * 
+ * @param frontLeftPin The pin that the front left
+ * motor is on.
+ * @param frontRightPin The pin that the front right
+ * motor is on.
+ * @param rearLeftPin The pin that the rear left 
+ * motor is on.
+ * @param rearRightPin The pin that the rear right
+ * motor is on.
+ * @return Constructor, returns void.
+ */
 Drive::Drive(int frontLeftPin, int frontRightPin, 
 	int rearLeftPin, int rearRightPin)
 {
@@ -10,23 +26,63 @@ Drive::Drive(int frontLeftPin, int frontRightPin,
 	m_rearRightMotor.attach(rearRightPin);
 }
 
+/**
+ * @brief Drives the robot using tank drive.
+ * @details Drives the robot using the standard
+ * tank drive method, but takes in a PPM object
+ * to drive based on the input from a Vex 
+ * controller.
+ * 
+ * @param ppm The PPM object to read joysticks from.
+ */
 void Drive::TankDrive(PPM &ppm)
 {
 	this->TankDrive(map(ppm.getChannel(kLeftStickY), 0, 180, -90, 90), 
 		map(ppm.getChannel(kRightStickY), 0, 180, -90, 90));
 }
 
+/**
+ * @brief Drives the robot using tank drive.
+ * @details Drives the robot using the standard
+ * tank drive method, taking in a value for the
+ * speed of the right side and the speed of the
+ * left side.
+ * 
+ * @param rightSpeed The speed to drive the right
+ * side (-90 to 90).
+ * @param leftSpeed The speed to drive the left
+ * side (-90 to 90).
+ */
 void Drive::TankDrive(int rightSpeed, int leftSpeed)
 {
 	this->PowerMotors(leftSpeed, rightSpeed, leftSpeed, rightSpeed);
 }
 
+/**
+ * @brief Drives the robot using arcade drive.
+ * @details Drives the robot using the standard
+ * arcade drive method, but takes in a PPM object
+ * to drive based on the input from a Vex 
+ * controller.
+ * 
+ * @param ppm The PPM object to read joysticks from.
+ */
 void Drive::ArcadeDrive(PPM &ppm)
 {
 	this->ArcadeDrive(map(ppm.getChannel(kLeftStickY), 0, 180, -90, 90), 
 		map(ppm.getChannel(kRightStickX), 0, 180, -90, 90));
 }
 
+/**
+ * @brief Drives the robot using arcade drive.
+ * @details Drives the robot using the standard 
+ * arcade drive method. Takes in a value for the 
+ * speed of forward motion, and a value for the
+ * robot rotation speed.
+ * 
+ * @param moveValue Forward motion speed (-90 to 90).
+ * @param rotateValue Rotation speed (-90 to 90).
+ */
 void Drive::ArcadeDrive(int moveValue, int rotateValue)
 {
 	int leftSpeed, rightSpeed = constrain(moveValue, 0, 90);
@@ -44,12 +100,35 @@ void Drive::ArcadeDrive(int moveValue, int rotateValue)
 	this->PowerMotors(leftSpeed, rightSpeed, leftSpeed, rightSpeed);
 }
 
+/**
+ * @brief Drive wheels individually.
+ * @details Drive the drive wheels individually.
+ * All values should be input in the range -90 to 90.
+ * 
+ * @param frontLeft The speed of the front left wheel.
+ * @param frontRight The speed of the front right wheel.
+ * @param rearLeft The speed of the rear left wheel.
+ * @param rearRight The speed of the rear right wheel.
+ */
 void Drive::DriveIndividual(int frontLeft, int frontRight, 
 		int rearLeft, int rearRight)
 {
 	PowerMotors(frontLeft, frontRight, rearLeft, rearRight);
 }
 
+/**
+ * @brief Power the motors.
+ * @details This method actually drives the motors.
+ * This is the only method that should be used for actually
+ * interfacing with the motors. The method does math
+ * such that the motors will all drive in the same 
+ * direction if given the same values.
+ * 
+ * @param frontLeftSpeed The speed of the front left wheel.
+ * @param frontRightSpeed The speed of the front right wheel.
+ * @param rearLeftSpeed The speed of the rear left wheel.
+ * @param rearRightSpeed The speed of the rear right wheel.
+ */
 void Drive::PowerMotors(int frontLeftSpeed, int frontRightSpeed,
 		int rearLeftSpeed, int rearRightSpeed)
 {
--- Robotmap.h
+++ Robotmap.h
@@ -1,22 +1,36 @@
 #ifndef ROBOTMAP_H
 #define ROBOTMAP_H
 
-enum motors
+///The different motors that are attached to the robot.
+enum Motors
 {
-	kFrontLeftMotor = 1,
-	kFrontRightMotor,
-	kRearLeftMotor,
-	kRearRightMotor,
-	kArmMotor,
-	kFeederMotor
+	kFrontLeftMotor = 1, ///< The front left motor.
+	kFrontRightMotor, ///< The front right motor.
+	kRearLeftMotor, ///< The rear left motor.
+	kRearRightMotor, ///< The rear right motor.
+	kArmMotor, ///< The arm motor.
+	kFeederMotor ///< The feeder motor.
 };
 
+///The digital inputs on the robot.
+enum DigitalInputs
+{
+	kDIO1 = 1
+};
+
+///The Analog inputs on the robot.
+enum AnalogInputs
+{
+	kAI1 = A1
+};
+
+///The joystick axis on the robot.
 enum JoystickAxis
 {
-	kRightStickX = 1,
-	kRightStickY,
-	kLeftStickY,
-	kLeftStickX
+	kRightStickX = 1, ///< The right joystick's X axis.
+	kRightStickY, ///< The right joystick's Y axis.
+	kLeftStickY, ///< The left joystick's Y axis.
+	kLeftStickX ///< The left joystick's X axis.
 };
 
 #endif
